a441bef844f60061d7640cbd6ad43a1fa42cc244
Nicholas-Westley <ms0u108c@hotmail.com>
2015-09-08 09:57:12 +0700

    include toasts to inform user about example creation

--- app/controllers/c_main.js
+++ app/controllers/c_main.js
@@ -1,7 +1,7 @@
 
 var thaiFonts = angular.module('ThaiFonts', ['ngMaterial']);
 
-thaiFonts.controller('MainCtrl', function ($scope, $http) {
+thaiFonts.controller('MainCtrl', function ($scope, $http, $mdToast, $animate) {
 	
 	$scope.thaiInput = "";
 	$scope.newExampleText = ""
@@ -14,11 +14,9 @@ thaiFonts.controller('MainCtrl', function ($scope, $http) {
 		"SarunsManorah",
 		"TorsilpWadkhen"
 	];
+
 	$scope.examples = [];
-
-
 	$scope.getExamples = function() {
-
 		$http.get('/api/examples/')
 			.success(function(data) {
 				console.log(data);
@@ -27,29 +25,38 @@ thaiFonts.controller('MainCtrl', function ($scope, $http) {
 				console.log('Error: ' + data);
 			});
 	}
-
 	$scope.getExamples();
+
+	$scope.toast = function(msg, time) {
+		$mdToast.show(
+      		$mdToast.simple()
+        	.content(msg)
+        	.position("top right")
+        	.hideDelay(time)
+		);
+	};
 
 
 	$scope.addExample = function() {
 
-		if($scope.newExampleText === "" || $scope.newExampleText.length < 5) {
+		if($scope.newExampleText === "" || $scope.newExampleText.length < 10) {
+			$scope.toast("Example must be at least 10 characters", 3000);
 			return;
 		}
 
 		$http.post('/api/example', {content: $scope.newExampleText})
 			.success(function(example) {
 				$scope.newExampleText = "";
+				$scope.toast('Example Added To Database', 1000);
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);
 			});
 	};
 
-
-
-
 });
+
+
 
 
 
